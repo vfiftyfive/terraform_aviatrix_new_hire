a9e694b86be4290e57eceb1afef14eab329c93f5
Nicolas Vermande <nvermande@aviatrix.com>
2021-05-09 17:06:36 +0100

    Change provider syntax

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "aviatrix" {
-  //get controller IP from remote state - contoller bootstrap plan
+  //get controller IP from remote state - contoller bootstrap config
   controller_ip = data.terraform_remote_state.controller_data.outputs.cloudformation["AviatrixControllerEIP"]
   username      = "admin"
-  //get updated password from remote state - controller bootstrap plan
+  //get updated password from remote state - controller bootstrap config
   // password = data.terraform_remote_state.controller_data.outputs.cloudformation["AviatrixControllerPrivateIP"]
-  password = "AV!@trix123"
+  password                = "AV!@trix123"
   skip_version_validation = true
 }
